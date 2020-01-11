734fd44414d5f72d7e20a3d21a6f09fa81c34bff
jasperbeckers <jasperbeckers@gmail.com>
2020-01-11 01:07:14 +0100

    Add welcome message

--- app/content.js
+++ app/content.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import BackgroundImage from "./background";
+import * as Font from "expo-font";
 
 class Content extends Component {
+  componentDidMount() {
+    Font.loadAsync({
+      "roboto-thin": require("../assets/fonts/Roboto-Thin.ttf")
+    });
+  }
+
   render() {
     return (
       <View style={styles.appstyle}>
         <BackgroundImage>
           <View Viewstyle={styles.textstyle}>
             <Text style={styles.title}>Coming Soon!</Text>
+            <Text style={styles.subtitle}>Your travel guide to anywhere</Text>
           </View>
         </BackgroundImage>
       </View>
@@ -28,10 +36,18 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     fontSize: 48,
-    fontFamily: "Helvetica",
+    fontFamily: "roboto-thin",
     marginVertical: 16,
     textAlign: "center",
-    color: "green"
+    color: "black"
+  },
+  subtitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    fontFamily: "roboto-thin",
+    marginVertical: 16,
+    textAlign: "center",
+    color: "black"
   }
 });
 
