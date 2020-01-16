87062818a98bd3a2e55beefef709adad7222ac89
jasperbeckers <jasperbeckers@gmail.com>
2020-01-17 00:11:27 +0100

    Revert babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [["env", {
-      "modules": "commonjs",
-      "targets": {
-        "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
-      }
-    }], 'babel-preset-expo'],
-	sourceType: 'unambiguous',
+    presets: ['babel-preset-expo'],
   };
 };
